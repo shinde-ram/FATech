478057206ff3d59a57242ee2bbc34f5b5e7041b6
Shinde Rameshwar <18ramshinde@gmail.com>
2025-07-05 18:02:36 +0530

    changes all the price 3

--- appwriteConfig.js
+++ appwriteConfig.js
@@ -3,7 +3,7 @@ import { Client, Databases } from "appwrite";
 const client = new Client();
 
 client
-    .setEndpoint("https://cloud.appwrite.io/v1") // or your custom endpoint
+    .setEndpoint("https://fra.cloud.appwrite.io/v1") // or your custom endpoint
     .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID); // Replace with actual ID
 
 const databases = new Databases(client);
